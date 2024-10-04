a8b450c4dbe90605ccc6544a540aa76bd3b355e2
VANISREE-220701308 <220701308@rajalakshmi.edu.in>
2024-10-04 20:32:25 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <title>Insert Data Form</title>
+</head>
+<body>
+    <h2>Insert Data</h2>
+    <form action="insertion1.php" method="POST">
+        <label for="ATYPE">ATYPE:</label>
+        <input type="text" id="ATYPE" name="ATYPE" required><br><br>
+
+        <label for="ANO">ANO:</label>
+        <input type="text" id="ANO" name="ANO" required><br><br>
+
+        <label for="BALANCE">BALANCE:</label>
+        <input type="number" id="BALANCE" name="BALANCE" required><br><br>
+
+        <label for="CID">CID:</label>
+        <input type="text" id="CID" name="CID" required><br><br>
+
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+
